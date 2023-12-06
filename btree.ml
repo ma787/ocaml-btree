1190c26b7f0e7ce1c778b9ce8c9d465bc7e954e6
Mohamed <mo501@cam.ac.uk>
2023-12-06 19:27:42 +0000

    Generalised the split function to a given key rather than the median. Added add_items function to replace accumulators in merge.

--- btree.ml
+++ btree.ml
@@ -50,6 +50,15 @@ let rec update_node tree k p c1 c2 = match tree with
   else raise (MalformedTree "key already in node")
 | _ -> raise (NullTree "")
 
+let rec add_items tree k p cn = match tree with
+| Lf ([], [], r, t) -> Lf (k::[], p::[], r, t)
+| Lf (v::next, pl::pls, r, t) -> Lf(k::v::next, p::pl::pls, r, t)
+| Il ([], [], [], r, t) -> 
+  if List.length cn < 2 then raise (MalformedTree "internal node must have >1 child")
+  else Il (k::[], p::[], cn, r, t)
+| Il (v::next, pl::pls, c, r, t) -> Il (k::v::next, p::pl::pls, c @ cn, r, t)
+| _ -> raise (MalformedTree "keys/payloads/children mismatch")
+
 (* splits a root node into three *)
 (* resulting in a new root and increasing the tree depth by 1 *)
 let rec split_root tree = match tree with
@@ -64,16 +73,16 @@ let tr = Lf (get_right ks mk, get_right pls mp, false, t) in
 Il (mk::[], mp::[], tl::tr::[], true, t)
 | _ -> raise (NullTree "")
 
-(* splits a node in two on the median key *)
-(* migrates median key to parent node and returns parent, which may now be full *)
-let rec split tree parent = match tree, parent with
+(* splits a node in two on a given key index *)
+(* migrates key to parent node and returns parent, which may now be full *)
+let rec split tree parent m = match tree, parent with
 | Il (ks, pls, c::cn, r, t), Il (ks1, pl1, cn1, r1, t1) ->
-  let mk, mp, mc = List.nth ks (t-1), List.nth pls (t-1), List.nth cn (t-2) in
+  let mk, mp, mc = List.nth ks m, List.nth pls m, List.nth cn m in
   let tl = Il (get_left ks mk, get_left pls mp, c::(get_left cn mc), false, t) in
   let tr = Il (get_right ks mk, get_right pls mp, mc::(get_right cn mc), false, t) in
   update_node parent mk mp tl tr
 | Lf (ks, pls, r, t), Il (ks1, pls1, cn1, r1, t1) ->
-  let mk, mp = List.nth ks (t-1), List.nth pls (t-1) in
+  let mk, mp = List.nth ks m, List.nth pls m in
   let tl = Lf (get_left ks mk, get_left pls mp, false, t) in
   let tr = Lf (get_right ks mk, get_right pls mp, false, t) in
   update_node parent mk mp tl tr
@@ -83,36 +92,44 @@ let rec split tree parent = match tree, parent with
 (* inserts a given key and payload into the tree *)
 let rec insert tree (k, p) = match tree with
 | Lf (v::next, pl::pls, true, t) ->
-  if (List.length(next)+1) == 2*t-1 then insert (split_root tree) (k, p)
+  if List.length(v::next) == 2*t-1 then insert (split_root tree) (k, p)
   else if k<v then Lf (k::v::next, p::pl::pls, true, t)
   else if k==v then Lf (v::next, p::pls, true, t) (* update payload *)
   else if next=[] then Lf (v::k::next, pl::p::pls, true, t)
   else insert (Lf (next, pls, true, t)) (k, p)
 | Il (v::next, pl::pls, c1::c2::cn, r, t) -> (* every non-leaf node must have at least 2 children *)
-  if (List.length(next)+1) == 2*t-1 then
+  if List.length(v::next) == 2*t-1 then
     if r then insert (split_root tree) (k, p) (* root is full *)
     else raise (MalformedTree "parent node cannot be full")
   else if k<v then match c1 with
     | Il (k1s, p1::pl1, cn1, r1, t) -> 
-      if List.length k1s == 2*t-1 then insert (split c1 tree) (k, p)
+      let l = List.length k1s in
+      let m = if l mod 2 == 0 then (l/2)+1 else (l+1)/2 in
+      if List.length k1s == 2*t-1 then insert (split c1 tree m) (k, p)
       else insert c1 (k, p)
     | Lf (v1::next1, p1::pl1, r1, t) -> 
-      if (List.length(next1)+1) == 2*t-1 then insert (split c1 tree) (k, p)
+      let l = List.length (v1::next1) in
+      let m = if l mod 2 == 0 then (l/2)+1 else (l+1)/2 in
+      if List.length(v1::next1) == 2*t-1 then insert (split c1 tree m) (k, p)
       else insert c1 (k, p)
     | _ -> raise (MalformedTree "internal node must have >1 child")
   else if k==v then Il (v::next, p::pls, c1::c2::cn, r, t) (* update payload *)
   else if next=[] then match c2 with (* rightmost child *)
     | Il (k2s, p2::pl2, cn2, r2, t) ->
-      if List.length k2s == 2*t-1 then insert (split c2 tree) (k, p)
+      let l = List.length (k2s) in
+      let m = if l mod 2 == 0 then (l/2)+1 else (l+1)/2 in
+      if List.length k2s == 2*t-1 then insert (split c2 tree m) (k, p)
       else insert c2 (k, p)
     | Lf (v2::next2, p2::pl2, r2, t) ->
-      if (List.length(next2)+1) == 2*t-1 then insert (split c2 tree) (k, p)
+      let l = List.length (v2::next2) in
+      let m = if l mod 2 == 0 then (l/2)+1 else (l+1)/2 in
+      if List.length(v2::next2) == 2*t-1 then insert (split c2 tree m) (k, p)
       else insert c2 (k, p)
     | _ -> raise (MalformedTree "internal node must have >1 child")
   else insert (Il (next, pls, c1::c2::cn, r, t)) (k, p)
 | _ -> raise (MalformedTree "internal node cannot be empty or without children")
 
-let rec merge parent acck accp accn s1 s2 = match parent with
+let rec merge parent s1 s2 = match parent with
 | Lf _ -> raise (MalformedTree "lead node cannot be parent")
 | Il(v::next, pl::pls, c1::c2::cn, r, t) -> 
   if (c1==s1 && c2==s2) then match s1, s2 with
@@ -123,13 +140,13 @@ let rec merge parent acck accp accn s1 s2 = match parent with
       let l = List.length(k1s) in 
       if (l < t-1 || l > 2*t-1) then raise (TreeCapacityNotMet "")
       else let s = Lf (km, pm, false, t) in
-      Il (acck @ next, accp @ pls, accn @ s::cn, r, t)
+      Il (next, pls, s::cn, r, t)
     | Il (k1s, p1s, cn1, false, t1), Il (k2s, p2s, cn2, false, t2) ->
       let km, pm, cm = k1s @ [v] @ k2s, p1s @ [pl] @ p2s, cn1 @ cn2 in
       let l = List.length(k1s) in
       if (l < t-1 || l > 2*t-1) then raise (TreeCapacityNotMet "")
       else let s = Il (km, pm, cm, false, t) in
-      Il (acck @ next, accp @ pls, accn @ s::cn, r, t)
+      Il (next, pls, s::cn, r, t)
     | _, _ -> raise (MalformedTree "child nodes cannot be empty")
-  else merge (Il (next, pls, cn, r, t)) (v::acck) (pl::accp) (c1::c2::accn) s1 s2
+  else add_items (merge (Il (next, pls, cn, r, t)) s1 s2) v pl [c1;c2]
 | _ -> raise (NullTree "") (* occurs if s1 and s2 are not children of parent *)
