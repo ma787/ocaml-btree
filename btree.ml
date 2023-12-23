be7a13cabfd098bb2834173c9ed58907bd4093e4
Mohamed <mo501@cam.ac.uk>
2023-12-23 16:21:21 +0000

    Added a length parameter to delete_aux. Fixed bug in del_next.

--- btree.ml
+++ btree.ml
@@ -234,13 +234,14 @@ let rec swap_successor tree k1 p1 k2 p2 i = match tree with
 
 let rec delete tree k =
   let eq a b = a=b in 
-  let rec delete_aux tree parent k =
+  let rec delete_aux tree parent k l =
     let del_next tnode nk nv npl nc  = 
-      let p, oldt, tnext = delete_aux tnode parent k in (match tnext with
-      | Il ([], [], c::[], r, t) -> p, oldt, (restore tnext nv npl nc)
-      | Il (v::next, pl::pls, c::cn, r, t) -> if List.exists (eq nk) (v::next) then p, oldt, (restore tnext nv npl nc) else p, oldt, tnext
-      | Lf ([], [], r, t) -> p, oldt, (restore tnext nv npl nc)
-      | Lf (v::next, pl::pls, r, t) -> if List.exists (eq nk) (v::next) then p, oldt, (restore tnext nv npl nc) else p, oldt, tnext
+      let res tree = restore tree nv npl nc in
+      let p, oldt, tnext = delete_aux tnode parent k (l+1) in (match tnext with
+      | Il ([], [], c::[], r, t) -> p, res oldt, res tnext
+      | Il (v::next, pl::pls, c::cn, r, t) -> if List.exists (eq nk) (v::next) then p, res oldt, res tnext else p, res oldt, tnext
+      | Lf ([], [], r, t) -> p, res oldt, res tnext
+      | Lf (v::next, pl::pls, r, t) -> if List.exists (eq nk) (v::next) then p, res oldt, (restore tnext nv npl nc) else p, res oldt, tnext
       | _ -> raise (MalformedTree "no match found")) in 
     match tree, parent with
     | _, Lf _ ->
@@ -249,15 +250,15 @@ let rec delete tree k =
           let tnext = Lf (next, pls, true, t) in
           if k=v then tnext, parent, tnext
           else if k>v then 
-            let a, b, c = delete_aux tnext tnext k in
+            let a, b, c = delete_aux tnext tnext k (l+1) in
             let newt = restore a v pl (Lf ([], [], false, 0)) in newt, parent, newt
           else raise (NotFound "key not found")
         | Il (v::next, pl::pls, c::cn, true, t) -> 
-          let tnext = Il (next, pls, cn, true, t) in
           if k=v then
-          let swapt = (let k1, p1 = (find_successor tree v pl false) in swap_successor tree v pl k1 p1 false) in delete_aux swapt (replace_child parent tree swapt) k
+          let swapt = (let k1, p1 = (find_successor tree v pl false) in swap_successor tree v pl k1 p1 false) in delete_aux swapt (replace_child parent tree swapt) k 0
           else if k>v then 
-            let a, b, c = delete_aux tnext tnext k in
+            let tnext = Il (next, pls, cn, true, t) in
+            let a, b, c = delete_aux tnext tnext k (l+1) in
             let newt = restore a v pl c in newt, parent, newt
           else raise (NotFound "key not found")
         | _ -> raise (NotFound "key not found"))
@@ -266,32 +267,32 @@ let rec delete tree k =
     | Lf (v::next, pl::pls, r, t), _ ->
       if k<v then raise (NotFound "key not found")
       else if k=v then
-        if List.length (v::next) < t then
+        if l + (List.length (v::next)) < t then
           let newp = refill tree parent in
-          let newc = search newp k in delete_aux newc newp k
+          let newc = search newp k in delete_aux newc newp k 0
         else parent, tree, (Lf (next, pls, r, t))
       else del_next (Lf (next, pls, r, t)) k v pl (Lf ([], [], false, 0))
     | Il (_, _, c::[], r, t), _ -> raise (NotFound "key not found")
     | Il (v::next, pl::pls, c1::c2::cn, r, t), _ ->
-      if k<v then let p, oldt, newc = delete_aux c1 tree k in p, oldt, (Il (v::next, pl::pls, newc::c2::cn, r, t))
+      if k<v then let p, oldt, newc = delete_aux c1 tree k 0 in p, oldt, (Il (v::next, pl::pls, newc::c2::cn, r, t))
       else if k>v then del_next (Il (next, pls, (c2::cn), r, t)) k v pl c1
       else 
-        let swapt = let k1, p1 = find_successor tree v pl false in swap_successor tree v pl k1 p1 false in delete_aux swapt (replace_child parent tree swapt) k
+        let swapt = let k1, p1 = find_successor tree v pl false in swap_successor tree v pl k1 p1 false in delete_aux swapt (replace_child parent tree swapt) k 0
     | _, _ -> raise (NullTree "") in
-  let delete_final tree parent k = 
-    let p, oldt, newt = delete_aux tree parent k in
+  let delete_final tree parent k l = 
+    let p, oldt, newt = delete_aux tree parent k 0 in
     if p=newt then p else replace_child p oldt newt in
   let tdel = search tree k in
   if tree=tdel then match tree with
-    | Lf (_, _, true, t) -> delete_final tree tdel k
-    | Il (_, _, _, true, t) -> delete_final tree tdel k
+    | Lf (_, _, true, t) -> delete_final tree tdel k 0
+    | Il (_, _, _, true, t) -> delete_final tree tdel k 0
     | _ -> raise (MalformedTree "tree and parent required for deletion if not root")
   else match tree with
     | Il (v::next, pl::pls, c::cn, r, t) ->
-      if c=tdel then delete_final tdel tree k else match c with
+      if c=tdel then delete_final tdel tree k 0 else match c with
       | Lf _ -> restore (delete (Il (next, pls, cn, r, t)) k) v pl c
-      | Il (k1::k1s, p1::p1s, c1::cn1, r, t) -> 
+      | Il (k1::k1s, p1::p1s, c1::cn1, r1, t) -> 
         if k<k1 then Il (v::next, pl::pls, (delete c k)::cn, r, t)
         else if k>k1 then restore (delete (Il (next, pls, cn, r, t)) k) v pl c
         else raise (NullTree "")
-    | Il ([], [], c::[], r, t) -> if c=tdel then delete_final tdel tree k else raise (NotFound "key not found")
+    | Il ([], [], c::[], r, t) -> if c=tdel then delete_final tdel tree k 0 else raise (NotFound "key not found")
