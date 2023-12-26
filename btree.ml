34fd45995140f3a302c6638f60c608eade401aca
Mohamed <mo501@cam.ac.uk>
2023-12-26 19:51:08 +0000

    Removed delete code and associated functions. Wrote functions that respectively find inorder predecessor and successor of a key in a tree.

--- btree.ml
+++ btree.ml
@@ -18,31 +18,31 @@ let string_of_tree_list l = "[" ^ (List.fold_left (fun acc x -> acc ^ (to_string
 
 let rec restore tree k p c = match tree with
 | Lf ([], [], r, t) -> Lf (k::[], p::[], r, t)
-| Lf (v::next, pl::pls, r, t) -> Lf(k::v::next, p::pl::pls, r, t)
-| Il ([], [], cn, r, t) -> Il(k::[], p::[], c::cn, r, t)
+| Lf (v::next, pl::pls, r, t) -> Lf (k::v::next, p::pl::pls, r, t)
+| Il ([], [], cn, r, t) -> Il (k::[], p::[], c::cn, r, t)
 | Il (v::next, pl::pls, cn, r, t) -> Il (k::v::next, p::pl::pls, c::cn, r, t)
 | _ -> raise (MalformedTree "keys/payloads/children mismatch")
 
 (* searches for a node with key k and returns node *)
 let rec search tree k = let eq a b = a=b in
-let search_next tnode nk nv npl nc  = let tnext = search tnode k in (match tnext with
+let search_next tnode ks nv npl nc  = let tnext = search tnode k in (match tnext with
 | Il ([], [], c::[], r, t) -> restore tnext nv npl nc
 | Il (v::next, pl::pls, c::cn, r, t) -> 
-  if List.exists (eq nk) (v::next) then restore tnext nv npl nc else tnext
+  if List.exists (eq v) ks then restore tnext nv npl nc else tnext
 | _ -> tnext) in
 match tree with
 | Il (v::next, pl::pls, c::cn, r, t) -> 
   if k=v then tree
   else if k<v then search c k
-  else search_next (Il (next, pls, cn, r, t)) k v pl c
+  else search_next (Il (next, pls, cn, r, t)) (v::next) v pl c
 | Il ([], [], c::[], r, t) -> search c k
 | Lf (v::next, pl::pls, r, t) ->
   if k=v then tree
   else if k>v then
-    if next=[] then raise (NotFound "key not found")
+    if next=[] then raise (NotFound ("key not found - " ^ string_of_int k ^ " - " ^ to_string tree))
     else restore (search (Lf (next, pls, r, t)) k) v pl (Lf ([], [], false, 0))
-  else raise (NotFound ("key not found"))
-| _ -> raise (NotFound ("key not found"))
+  else raise (NotFound "key not found")
+| _ -> raise (NotFound "key not found")
 
 let rec get_left l i m = match l with
 | c::cs -> if i=m then [] else c::(get_left cs (i+1) m)
@@ -162,137 +162,50 @@ let rec merge parent s1 s2 ignore = match parent with
   else restore (merge (Il (next, pls, (c2::cn), r, t)) s1 s2 ignore) v pl c1
 | _ -> raise (NotFound "could not find sibling nodes") (* occurs if s1 and s2 are not children of parent *)
 
-(* adds keys from a sibling node to B if possible *)
-  let rec redistribute tree parent = match parent with
-| Lf _ -> raise (MalformedTree "leaf node cannot be parent")
+let rec find_predecessor tree (k, p) i = match tree with
+| Lf (v::next, pl::pls, r, t) ->
+  if i then
+    if next=[] then (v, pl)
+    else find_predecessor (Lf (next, pls, r, t)) (k, p) i (* find largest key in leaf node *)
+  else
+    if k=v then raise (NotFound "") (* the predecessor is higher in the tree **)
+    else if next=[] then raise (NotFound "key not found")
+    else if List.hd next = k then (v, pl)
+    else find_predecessor (Lf (next, pls, r, t)) (k, p) i
 | Il (v::next, pl::pls, c1::c2::cn, r, t) ->
-  if c1=tree then match c2 with
-    | Lf (ks, pls, r, t) -> 
-      let l = List.length ks in
-      if l > t-1 then split (merge parent tree c2 true) parent (l-1)
-      else raise (NullTree "")
-    | Il (ks, pls, cn, r, t) ->
-      let l = List.length ks in
-      if l > t-1 then split (merge parent tree c2 true) parent (l-1)
-      else raise (NullTree "")
-  else if c2=tree then match c1 with
-    | Lf (ks, pls, r, t) -> 
-      let l = List.length ks in
-      if l > t-1 then split (merge parent c1 tree true) parent (l-1)
-      else raise (NullTree "")
-    | Il (ks, pls, cn, r, t) ->
-      let l = List.length ks in
-      if l > t-1 then split (merge parent c1 tree true) parent (l-1)
-      else raise (NullTree "")
-  else restore (redistribute tree (Il (next, pls, cn, r, t))) v pl c1
-| _ -> raise (NotFound "child not found")
+  if not i then
+    if k=v then find_predecessor c1 (k, p) true
+    else if k<v then find_predecessor c1 (k,p) i
+    else if (next=[] || k < List.hd next) then 
+      (try find_predecessor c2 (k, p) i 
+      with (NotFound "") -> (v, pl))
+    else find_predecessor (Il (next, pls, (c2::cn), r, t)) (k, p) i
+  else
+    if cn=[] then find_predecessor c2 (k, p) true
+    else find_predecessor (Il (next, pls, (c2::cn), r, t)) (k, p) i
+| _ -> raise (NotFound "key or predecessor not found")
 
-(* attempts to redistribute keys between B and its sibling nodes*)
-(* otherwise merges B with a sibling to make sure it has enough children*)
-let rec refill tree parent = try redistribute tree parent with (NullTree "") ->
-  match parent with
-  | Lf _ -> raise (MalformedTree "leaf node cannot be parent")
-  | Il (v::next, pl::pls, c1::c2::cn, r, t) ->
-    if c1=tree then merge parent tree c2 false
-    else if c2=tree then merge parent c1 tree false
-    else raise (MalformedTree "")
-  | _ -> raise (MalformedTree "")
-
-let rec replace_child parent old newc = match parent with
-| Lf _ -> raise (MalformedTree "leaf node cannot be parent")
-| Il ([], [], _, r, t) -> raise (NotFound "child not found")
-| Il (v::next, pl::pls, c::cn, r, t) ->
-  if c=old then Il (v::next, pl::pls, newc::cn, r, t)
-  else restore (replace_child (Il (next, pls, cn, r, t)) old newc) v pl c
-| _ -> raise (NullTree "")
-
-(* returns the key that is the inorder successor of k in tree, along with payload *)
-let rec find_successor tree k p i = match tree with
-| Lf _ -> raise (MalformedTree "leaf node cannot be parent")
-| Il (v::next, pl::pls, c1::c2::cn, r, t) -> let m = k=v in
-  if (m && not i) then match c2 with
-    | Lf (k1::k1s, p1::p1s, r, t) -> k1, p1
-    | Il (v1::next1, p1::p1s, ca::cb::cn1, r, t) -> find_successor c2 v1 p1 true
-    | _ -> raise (MalformedTree "child node cannot be empty")
-  else if m then match c1 with
-    | Lf (k1::k1s, p1::p1s, r, t) -> k1, p1
-    | Il (v1::next1, p1::p1s, ca::cb::cn1, r, t) -> find_successor c1 v1 p1 true
-    | _ -> raise (MalformedTree "child node cannot be empty")
-  else find_successor (Il (next, pls, (c2::cn), r, t)) k p i
-| Il _ -> raise (NotFound "key not found")
-
-let rec swap_successor tree k1 p1 k2 p2 i = match tree with
-| Lf (v::next, pl::pls, r, t) -> let m = k1=v in
-  if (m && i) then Lf (k2::next, p2::pls, r, t)
-  else if m then restore (swap_successor (Lf (next, pls, r, t)) k1 p1 k2 p2 i) v pl (Lf ([], [], false, 0))
-  else Lf (k2::next, p2::pls, r, t)
-| Il (v::next, pl::pls, c1::c2::cn, r, t) -> let m = k1=v in
-  if (m && not i) then Il (k2::next, p2::pls, c1::(swap_successor c2 k2 p2 k1 p1 true)::cn, r, t)
-  else if m then Il (v::next, pl::pls, (swap_successor c1 k1 p1 k2 p2 i)::c2::cn, r, t)
-  else restore (swap_successor (Il (next, pls, (c2::cn), r, t)) k1 p1 k2 p2 i) v pl c1
-| _ -> raise (NotFound "at least one key to swap not found")
-
-let rec delete tree k =
-  let eq a b = a=b in 
-  let rec delete_aux tree parent k l =
-    let del_next tnode nk nv npl nc  = 
-      let res tree = restore tree nv npl nc in
-      let p, oldt, tnext = delete_aux tnode parent k (l+1) in (match tnext with
-      | Il ([], [], c::[], r, t) -> p, res oldt, res tnext
-      | Il (v::next, pl::pls, c::cn, r, t) -> if List.exists (eq nk) (v::next) then p, res oldt, res tnext else p, res oldt, tnext
-      | Lf ([], [], r, t) -> p, res oldt, res tnext
-      | Lf (v::next, pl::pls, r, t) -> if List.exists (eq nk) (v::next) then p, res oldt, (restore tnext nv npl nc) else p, res oldt, tnext
-      | _ -> raise (MalformedTree "no match found")) in 
-    match tree, parent with
-    | _, Lf _ ->
-      if tree=parent then (match parent with
-        | Lf (v::next, pl::pls, true, t) ->
-          let tnext = Lf (next, pls, true, t) in
-          if k=v then tnext, parent, tnext
-          else if k>v then 
-            let a, b, c = delete_aux tnext tnext k (l+1) in
-            let newt = restore a v pl (Lf ([], [], false, 0)) in newt, parent, newt
-          else raise (NotFound "key not found")
-        | Il (v::next, pl::pls, c::cn, true, t) -> 
-          if k=v then
-          let swapt = (let k1, p1 = (find_successor tree v pl false) in swap_successor tree v pl k1 p1 false) in delete_aux swapt (replace_child parent tree swapt) k 0
-          else if k>v then 
-            let tnext = Il (next, pls, cn, true, t) in
-            let a, b, c = delete_aux tnext tnext k (l+1) in
-            let newt = restore a v pl c in newt, parent, newt
-          else raise (NotFound "key not found")
-        | _ -> raise (NotFound "key not found"))
-        else raise (MalformedTree "leaf node cannot be parent")
-    | Lf ([], [], r, t), _ -> raise (NotFound "key not found")
-    | Lf (v::next, pl::pls, r, t), _ ->
-      if k<v then raise (NotFound "key not found")
-      else if k=v then
-        if l + (List.length (v::next)) < t then
-          let newp = refill tree parent in
-          let newc = search newp k in delete_aux newc newp k 0
-        else parent, tree, (Lf (next, pls, r, t))
-      else del_next (Lf (next, pls, r, t)) k v pl (Lf ([], [], false, 0))
-    | Il (_, _, c::[], r, t), _ -> raise (NotFound "key not found")
-    | Il (v::next, pl::pls, c1::c2::cn, r, t), _ ->
-      if k<v then let p, oldt, newc = delete_aux c1 tree k 0 in p, oldt, (Il (v::next, pl::pls, newc::c2::cn, r, t))
-      else if k>v then del_next (Il (next, pls, (c2::cn), r, t)) k v pl c1
-      else 
-        let swapt = let k1, p1 = find_successor tree v pl false in swap_successor tree v pl k1 p1 false in delete_aux swapt (replace_child parent tree swapt) k 0
-    | _, _ -> raise (NullTree "") in
-  let delete_final tree parent k l = 
-    let p, oldt, newt = delete_aux tree parent k 0 in
-    if p=newt then p else replace_child p oldt newt in
-  let tdel = search tree k in
-  if tree=tdel then match tree with
-    | Lf (_, _, true, t) -> delete_final tree tdel k 0
-    | Il (_, _, _, true, t) -> delete_final tree tdel k 0
-    | _ -> raise (MalformedTree "tree and parent required for deletion if not root")
-  else match tree with
-    | Il (v::next, pl::pls, c::cn, r, t) ->
-      if c=tdel then delete_final tdel tree k 0 else match c with
-      | Lf _ -> restore (delete (Il (next, pls, cn, r, t)) k) v pl c
-      | Il (k1::k1s, p1::p1s, c1::cn1, r1, t) -> 
-        if k<k1 then Il (v::next, pl::pls, (delete c k)::cn, r, t)
-        else if k>k1 then restore (delete (Il (next, pls, cn, r, t)) k) v pl c
-        else raise (NullTree "")
-    | Il ([], [], c::[], r, t) -> if c=tdel then delete_final tdel tree k 0 else raise (NotFound "key not found")
+let rec find_successor tree (k, p) i = match tree with
+| Lf (v::next, pl::pls, r, t) ->
+  if i then (v, pl)
+  else if r then
+    if next=[] then raise (NotFound "key or successor not found")
+    else if k=v then find_successor (Lf (next, pls, r, t)) (k, p) true
+    else find_successor (Lf (next, pls, r, t)) (k, p) i
+  else
+    if next=[] then 
+      if k=v then raise (NotFound "") (* the successor is higher in the tree *)
+      else raise (NotFound "key not found")
+    else if k=v then find_successor (Lf (next, pls, r, t)) (k, p) true
+    else find_successor (Lf (next, pls, r, t)) (k, p) i
+| Il (v::next, pl::pls, c1::c2::cn, r, t) -> 
+  if not i then
+    if k=v then find_successor c2 (k, p) true
+    else if k<v then 
+      (try find_successor c1 (k, p) i 
+      with (NotFound "") -> (v, pl))
+    else if next=[] then find_successor c2 (k, p) i
+    else find_successor (Il (next, pls, (c2::cn), r, t)) (k, p) i
+  else
+    find_successor c1 (k, p) i
+| _ -> raise (NotFound "key or predecessor not found")
